4eb20f04cb0fbd6e4b5705adc31f55936638b301
bhsohn <hanmuson1@gmail.com>
2024-05-10 13:21:46 +0900

    blazor 게시판 저장소 추가

---
+++ ArticleApp/ArticleApp.Models/ArticleRepository.cs
@@ -0,0 +1,40 @@
+﻿
+using Dul.Domain.Common;
+
+namespace ArticleApp.Models
+{
+    /// <summary>
+    /// Repository Class: ADO.net 또는 Dapper 또는 Entity Framework Core
+    /// </summary>
+    public class ArticleRepository : IArticleRepository
+    {
+        public Task<Article> AddArticleAsyn(Article article)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteArticleAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Article> EditArticleAsync(Article article)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Article> GetAritleByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Article>> GetArticlesAsync()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<PagingResult<Article>> GetAllAsync(int pageIndex, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ArticleApp/ArticleApp.Models/IArticleRepository.cs
@@ -0,0 +1,19 @@
+﻿
+using Dul.Domain.Common;
+
+namespace ArticleApp.Models
+{
+    /// <summary>
+    /// Repository 인터페이스
+    /// </summary>
+    public interface IArticleRepository
+    {
+        Task<Article> AddArticleAsyn(Article article); // 입력 
+        Task<List<Article>> GetArticlesAsync();         // 출력
+        Task<Article> GetAritleByIdAsync(int id);       // 상세
+        Task<Article> EditArticleAsync(Article article);    // 수정
+        Task DeleteArticleAsync(int id);            // 삭제
+
+        Task<PagingResult<Article>> GetAllAsync(int pageIndex, int pageSize); // 페이징 처리
+    }
+}
