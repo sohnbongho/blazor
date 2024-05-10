2a511d3ced488560c5e7cc31d752741376a76215
bhsohn <hanmuson1@gmail.com>
2024-05-10 13:57:41 +0900

    blazor 게시판 저장소 추가

---
+++ ArticleApp/ArticleApp.Models/ArticleAppDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+namespace ArticleApp.Models
+{
+    /// <summary>
+    /// DbContext Class
+    /// </summary>
+
+    public class ArticleAppDbContext : DbContext
+    {
+        // Install-Package Microsoft.EntityFrameworkCore.SqlServer    
+        // Install-Package Microsoft.EntityFrameworkCore.InMemory
+        // Install-Package System.Configuration.ConfigurationManager
+
+        public ArticleAppDbContext()
+        {
+            // Emplty
+            
+        }
+        public ArticleAppDbContext(DbContextOptions<ArticleAppDbContext> options)
+            : base(options) 
+        {
+            // 공식과 같은 코드
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // 닷넷 프레임워크 기반에서 호출되는 코드 영역: 
+            // App.config 또는 Web.config의 연결 문자열 사용
+            // 직접 데이터베이스 연결문자열 설정 가능
+            if (!optionsBuilder.IsConfigured)
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings[
+                    "ConnectionString"].ConnectionString;
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //// [A] Articles 테이블의 Created 열은 자동으로 GetDate() 제약 조건을 부여하기 
+            modelBuilder.Entity<Article>().Property(m => m.Created).HasDefaultValueSql("GetDate()");
+        }
+
+        // ArticleApp 관련 모든 테이블 참조
+        public DbSet<Article> Articles { get; set; }
+    }
+}
--- ArticleApp/ArticleApp.SqlServer/dbo/Tables/Articles.sql
+++ ArticleApp/ArticleApp.SqlServer/dbo/Tables/Articles.sql
@@ -2,6 +2,12 @@
 CREATE TABLE [dbo].[Articles]
 (
 	[Id] Int Not Null Primary Key Identity(1,1),	-- 일련번호
-	[Title] NVarChar(255) Not Null					-- 제목
+	[Title] NVarChar(255) Not Null,					-- 제목
+
+	-- AuditableBase.cs 참조
+	[CreatedBy] NVarChar(255) Null,				-- 등록자(Creator)
+	Created DateTime Default(GetDate()),			-- 생성일
+	ModifiedBy NVarChar(255) Null,				-- 수정자(ModifiedBy)
+	Modified DateTime Default(GetDate()),			-- 수정일
 )
 Go
