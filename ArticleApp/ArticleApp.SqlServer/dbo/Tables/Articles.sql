a928801547fd61a7e14b3d30758ab7c7cd317769
bhsohn <hanmuson1@gmail.com>
2024-05-13 18:03:40 +0900

    공지글 올리기

--- ArticleApp/ArticleApp.SqlServer/dbo/Tables/Articles.sql
+++ ArticleApp/ArticleApp.SqlServer/dbo/Tables/Articles.sql
@@ -4,7 +4,10 @@ CREATE TABLE [dbo].[Articles]
 	[Id] Int Not Null Primary Key Identity(1,1),	-- 일련번호
 	[Title] NVarChar(255) Not Null,					-- 제목
 
-	[Content] NVarChar(Max) Null,					-- 내용
+	[Content] NVarChar(Max) Null,					-- 내용 TODO => Not Null로
+
+	[IsPinned] Bit Null Default(0),					-- 공지글로 올리기
+
 
 	-- AuditableBase.cs 참조
 	[CreatedBy] NVarChar(255) Null,				-- 등록자(Creator)
