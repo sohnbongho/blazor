000b84790ebc93e5e724569f4a406009f75feb4a
bhsohn <hanmuson1@gmail.com>
2024-05-16 00:13:01 +0900

    program.cs 수정

---
+++ AdminApp/AdminApp/Infrastructures/DatabaseHelper.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace VisualAcademy;
+
+public class DatabaseHelper
+{
+    public static async Task AddOrUpdateRegistrationDate(string connectionString)
+    {
+        using (var con = new SqlConnection(connectionString))
+        {
+            await con.OpenAsync();
+
+            // 컬럼 추가
+            var alterCmd = new SqlCommand
+            {
+                Connection = con,
+                CommandText = @"
+                        IF NOT EXISTS (
+                            SELECT * FROM INFORMATION_SCHEMA.COLUMNS 
+                            WHERE TABLE_NAME = N'AspNetUsers' AND COLUMN_NAME = N'RegistrationDate'
+                        )
+                        BEGIN
+                            ALTER TABLE AspNetUsers ADD RegistrationDate DATETIMEOFFSET NULL;
+                        END;
+                    ",
+                CommandType = System.Data.CommandType.Text
+            };
+            await alterCmd.ExecuteNonQueryAsync();
+
+            // 업데이트
+            var updateCmd = new SqlCommand
+            {
+                Connection = con,
+                CommandText = @"
+                        UPDATE AspNetUsers
+                        SET RegistrationDate = SYSDATETIMEOFFSET()
+                        WHERE RegistrationDate IS NULL;
+                    ",
+                CommandType = System.Data.CommandType.Text
+            };
+            await updateCmd.ExecuteNonQueryAsync();
+        }
+    }
+}
--- AdminApp/AdminApp/Program.cs
+++ AdminApp/AdminApp/Program.cs
@@ -1,69 +1,190 @@
 using AdminApp.Areas.Identity;
 using AdminApp.Data;
+using AdminApp.Infrastructures;
+using AdminApp.Models;
+using DotNetNote.Models;
+using Hawaso.Models.CommonValues;
+using Hawaso.Models.Notes;
+using Hawaso.Models;
+using Hawaso.Settings;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using VisualAcademy;
 
-namespace AdminApp
+namespace AdminApp;
+public class Program
 {
-    public class Program
+    public static async Task Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+        var services = builder.Services;
+        var Configuration = builder.Configuration;
+
+        services.AddHttpContextAccessor();
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+            options => options.EnableRetryOnFailure()));
+
+        // ASP.NET Core Identity 설정
+        services.AddIdentity<IdentityUser, ApplicationRole>(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            options.SignIn.RequireConfirmedAccount = false;
+            options.SignIn.RequireConfirmedEmail = false;
+        })
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddDefaultTokenProviders();
 
-            // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+        services.Configure<IdentityOptions>(options =>
+        {
+            options.Password.RequiredLength = 4;
+        });
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews(); // View에서 가져옴
+        services.ConfigureApplicationCookie(options =>
+        {
+            options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+            options.ExpireTimeSpan = TimeSpan.FromMinutes(15);
+            options.LoginPath = "/Identity/Account/Login";
+            options.LogoutPath = "/Identity/Account/Logout";
+            options.ReturnUrlParameter = CookieAuthenticationDefaults.ReturnUrlParameter;
+            options.SlidingExpiration = true;
+        });
 
-            builder.Services.AddRazorPages();
-            builder.Services.AddServerSideBlazor();
-            builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
-            builder.Services.AddSingleton<WeatherForecastService>();
+        //builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+        //builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+        //    .AddEntityFrameworkStores<ApplicationDbContext>();
 
-            var app = builder.Build();
+        // Add services to the container.
+        services.AddControllersWithViews(); // View에서 가져옴
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseMigrationsEndPoint();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+        services.AddRazorPages();
+        services.AddServerSideBlazor()
+            .AddCircuitOptions(options => { options.DetailedErrors = true; });
 
-            app.UseHttpsRedirection();
+        // CORS 설정
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAnyOrigin",
+                builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+        });
+        services.AddHttpClient();
 
-            app.UseStaticFiles();
+        services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+        services.AddDatabaseDeveloperPageExceptionFilter();
+        builder.Services.AddSingleton<WeatherForecastService>();
 
-            app.UseRouting();
+        services.Configure<DotNetNoteSettings>(Configuration.GetSection("DotNetNoteSettings"));
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+        services.AddDbContext<DotNetNoteContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        services.AddDbContext<NoteDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            // Configure the HTTP request pipeline.
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+        // 의존성 주입 컨테이너 설정
+        //DependencyInjectionContainer(builder);
 
-            app.MapBlazorHub();
-            app.MapFallbackToPage("/_Host");
-            app.MapRazorPages();
+        //services.AddTransient<IFileStorageManager, ReplyAppFileStorageManager>();
+        //services.AddDependencyInjectionContainerForPurgeApp(Configuration.GetConnectionString("DefaultConnection"));
 
-            app.Run();
+        //// 데이터베이스 초기화 및 마이그레이션
+        //try
+        //{
+        //    await DatabaseHelper.AddOrUpdateRegistrationDate(connectionString);
+        //}
+        //catch (Exception)
+        //{
+
+        //}
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseMigrationsEndPoint();
         }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        // Configure the HTTP request pipeline.
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.MapBlazorHub();
+        app.MapFallbackToPage("/_Host");
+        app.MapRazorPages();
+
+        //using (var scope = app.Services.CreateScope())
+        //{
+        //    var scopedServices = scope.ServiceProvider;
+        //    await UserAndRoleInitializer.CreateBuiltInUsersAndRoles(scopedServices);
+        //}
+
+        app.Run();
+    }
+    // 의존성 주입 메서드 정의
+    private static void DependencyInjectionContainer(WebApplicationBuilder builder)
+    {
+        var services = builder.Services;
+        var Configuration = builder.Configuration;
+
+        services.AddSingleton<IConfiguration>(Configuration);
+
+        services.AddTransient<INoteRepository, NoteRepository>();
+        services.AddTransient<INoteCommentRepository, NoteCommentRepository>();
+
+        services.AddDbContext<HawasoDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")), ServiceLifetime.Transient);
+        services.AddDbContext<CommonValueDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        services.AddTransient<ICommonValueRepository, CommonValueRepository>();
+
+        // ManufacturerApp 관련 의존성 주입
+        //services.AddDependencyInjectionContainerForManufacturer(Configuration.GetConnectionString("DefaultConnection"));
+
+        //// Blazored.Toast
+        //services.AddBlazoredToast();
+
+        //// MemoApp 관련 의존성 주입
+        //services.AddDependencyInjectionContainerForMemoApp(Configuration.GetConnectionString("DefaultConnection"));
+
+        //// Upload Feature
+        //services.AddDiForLibries(Configuration.GetConnectionString("DefaultConnection"));
+        //services.AddDiForBriefingLogs(Configuration.GetConnectionString("DefaultConnection"));
+
+        //// ArchiveApp 관련 의존성 주입
+        //services.AddDependencyInjectionContainerForArchiveApp(Configuration.GetConnectionString("DefaultConnection"));
+
+        //#region VisualAcademy.Models.Departments.dll 
+        //// DepartmentApp 관련 의존성 주입
+        //services.AddDependencyInjectionContainerForDepartmentApp(Configuration.GetConnectionString("DefaultConnection"));
+        //#endregion
+
+        //builder.Services.AddDependencyInjectionContainerForBannedTypeApp(connectionString);
     }
 }
