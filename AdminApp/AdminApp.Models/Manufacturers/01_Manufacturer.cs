e791048e664e230504b6f9ca645e626ad2184bda
bhsohn <hanmuson1@gmail.com>
2024-05-15 22:18:29 +0900

    razor 프로젝트들 처리

---
+++ AdminApp/AdminApp.Models/Manufacturers/01_Manufacturer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hawaso.Models
+{
+    /// <summary>
+    /// 고객사 리스트 관리 앱
+    /// </summary>
+    public class Manufacturer
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string ManufacturerCode { get; set; }
+        public string Comment { get; set; }
+    }
+}
---
+++ AdminApp/AdminApp.Models/Projects/IProjectRepository.cs
@@ -0,0 +1,40 @@
+﻿using Dul.Articles;
+using Dul.Domain.Common;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Hawaso.Models
+{
+    /// <summary>
+    /// Repository Interface
+    /// </summary>
+    public interface IProjectRepository
+    {
+        Task<Project> AddProjectAsync(Project model);           // 입력
+        Task<List<Project>> GetProjectsAsync();                 // 출력
+        Task<Project> GetProjectByIdAsync(int id);              // 상세
+        Task<Project> EditProjectAsync(Project model);          // 수정
+        Task DeleteProjectAsync(int id);                        // 삭제
+
+        Task<PagingResult<Project>> GetAllAsync(
+            int pageIndex, int pageSize);                       // 페이징
+
+        // 필터링: 비동기 방식
+        Task<ArticleSet<Project, int>> GetArticles<TParentIdentifier>(
+            int pageIndex,
+            int pageSize,
+            string searchField,
+            string searchQuery,
+            string sortOrder,
+            TParentIdentifier parentIdentifier);
+
+        // 필터링: 동기 방식
+        ArticleSet<Project, int> GetArticlesSync<TParentIdentifier>(
+            int pageIndex,
+            int pageSize,
+            string searchField,
+            string searchQuery,
+            string sortOrder,
+            TParentIdentifier parentIdentifier);
+    }
+}
