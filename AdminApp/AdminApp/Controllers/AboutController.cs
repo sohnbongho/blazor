713c443e416ae11a788e7965e584a77cd4301fc2
bhsohn <hanmuson1@gmail.com>
2024-05-15 16:18:01 +0900

    데쉬 보드 처리

---
+++ AdminApp/AdminApp/Controllers/AboutController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminApp.Controllers
+{
+    public class AboutController : Controller
+    {        
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+    }
+}
