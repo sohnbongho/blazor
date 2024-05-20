b49eaadc3b3b2fac3a22c9ff6763555110a809a8
bhsohn <hanmuson1@gmail.com>
2024-05-20 14:37:13 +0900

    candidate db 저장 클래스 처리

--- Hawaso-study/Hawaso/Migrations/20240520052505_CandidateColumnAdd.Designer.cs
+++ Hawaso-study/Hawaso/Migrations/20240520052505_CandidateColumnAdd.Designer.cs
@@ -3,6 +3,7 @@ using Hawaso.Models.Candidates;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Hawaso.Migrations
 {
     [DbContext(typeof(CandidateAppDbContext))]
-    partial class CandidateAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240520052505_CandidateColumnAdd")]
+    partial class CandidateColumnAdd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Hawaso-study/Hawaso/Models/Candidates/01_Candidate.cs
+++ Hawaso-study/Hawaso/Models/Candidates/01_Candidate.cs
@@ -21,6 +21,59 @@ namespace Hawaso.Models.Candidates
     public class Candidate : CandidateBase
     {
         // Empty
+        [StringLength(35)]
+        public string? MiddleName { get; set; }
+        public string? DOB { get; set; }
+        public string? SSN { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [StringLength(254)]
+        public string? Email { get; set; }
+
+        [StringLength(70)]
+        public string? Address { get; set; }
+        
+        [StringLength(70)]
+        public string? City { get; set; }
+
+        [StringLength(2)]
+        public string? State { get; set; }
+
+        [DataType(DataType.PostalCode)]
+        [StringLength(35)]
+        public string? PostalCode { get; set; }
+
+        [StringLength(35)]
+        public string? Gender { get; set; }
+
+        [StringLength(70)]
+        public string? BirthCity { get; set; }
+
+        [StringLength(2)]
+        public string? BirthState { get; set; }
+        
+        [StringLength(70)]
+        public string? BirthCountry { get; set; }
+
+        [StringLength(35)]
+        public string? DriverLicenseNumber { get; set; }
+
+        [StringLength(2)]
+        public string? DriverLicenseState { get; set; }
+
+        public string? Photo { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [StringLength(35)]
+        public string? PrimaryPhone { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [StringLength(35)]
+        public string? SecondaryPhone { get; set; }
+
+        [StringLength(35)]
+        public string? LicenseNumber { get; set; }
+
     }
 
 }
