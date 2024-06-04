e0e3a19c1f596d3b8fc7318c7473603a12d34425
bhsohn <hanmuson1@gmail.com>
2024-06-04 12:03:29 +0900

    signalR 클라이언트 작업

--- signalR/SignalRStudy/SignalRStudy/MessageHub.cs
+++ signalR/SignalRStudy/SignalRStudy/MessageHub.cs
@@ -4,10 +4,10 @@ namespace SignalRStudy
 {
     public class MessageHub : Hub
     {
-        public async Task Send(string message)
+        public async Task Send(string message, string userId)
         {
             // 추가적인 작업가능
-            await Clients.All.SendAsync("receive", message);
+            await Clients.All.SendAsync("receive", message, userId);
         }
     }
 }
--- signalR/SignalRStudy/SignalRStudy/Program.cs
+++ signalR/SignalRStudy/SignalRStudy/Program.cs
@@ -5,9 +5,20 @@ namespace SignalRStudy
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddSignalR();
+
+            // Keep Alive시간 설정
+            builder.Services.AddSignalR().AddHubOptions<MessageHub>(options =>
+            {
+                options.EnableDetailedErrors = true;
+                // 30초 초과시, 서버에서 연결을 끊어버림
+                options.ClientTimeoutInterval = System.TimeSpan.FromSeconds(30);
+            });
+
 
             var app = builder.Build();
+            app.UseDefaultFiles(); // wwwroot기반으로 사용하겠다.
+            app.UseStaticFiles();
+
             app.MapGet("/", () => "Hello World!");
             app.MapHub<MessageHub>("/mychat");
 
