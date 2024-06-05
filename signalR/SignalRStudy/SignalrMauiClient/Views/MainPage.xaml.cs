2a0d91e1a73febd563180ead393f13e8b881f354
bhsohn <hanmuson1@gmail.com>
2024-06-05 15:27:20 +0900

    maui 플래폼 업데이트

---
+++ signalR/SignalRStudy/SignalrMauiClient/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SignalrMauiClient.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        
+    }
+
+}
