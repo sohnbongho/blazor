9d50a8f55207ecd95aa9d990852b50c9fade727f
bhsohn <hanmuson1@gmail.com>
2024-06-20 18:55:04 +0900

    파이프라인 추가

--- redis-study/rbay/sandbox/index.ts
+++ redis-study/rbay/sandbox/index.ts
@@ -2,13 +2,25 @@ import 'dotenv/config';
 import { client } from '../src/services/redis';
 
 const run = async () => {
-	await client.hSet('car', {
+	await client.hSet('car1', {
 		color: 'red',
 		year: 1950
 	});
+	await client.hSet('car2', {
+		color: 'green',
+		year: 1955
+	});
+	await client.hSet('car3', {
+		color: 'blue',
+		year: 1960
+	});
+	
+	const commands = [1,2,3].map((id) => {
+		return client.hGetAll('car'+id);
+	})
+	const results = await Promise.all( commands);
 
-	const car = await client.hGetAll('car');
+	console.log(results);
 
-	console.log(car);
 };
 run();
