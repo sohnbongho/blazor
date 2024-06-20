78b0d903fa4af0102800546e32dbc621298f5aa5
bhsohn <hanmuson1@gmail.com>
2024-06-20 16:26:38 +0900

    session 읽기 작업

--- redis-study/rbay/src/services/queries/sessions.ts
+++ redis-study/rbay/src/services/queries/sessions.ts
@@ -1,5 +1,25 @@
 import type { Session } from '$services/types';
+import { sessionsKey } from '$services/keys';
+import { client } from '$services/redis';
+import { object_without_properties } from 'svelte/internal';
 
-export const getSession = async (id: string) => {};
+export const getSession = async (id: string) => {
+    const session = await client.hGetAll(sessionsKey(id));
+
+    if(Object.keys(session).length === 0){
+        return null;
+    }
+    
+    return deserialize(id, session);
+};
 
 export const saveSession = async (session: Session) => {};
+
+
+const deserialize = (id: string, session : {[key:string]: string}) => {
+    return {
+        id, 
+        userId: session.userId,
+        username: session.username
+    }
+}
