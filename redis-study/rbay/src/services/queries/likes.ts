f4268376c574b3aaf28c9b971d5665b45b6d6bf7
bhsohn <hanmuson1@gmail.com>
2024-06-21 16:04:03 +0900

    좋아요 기능

--- redis-study/rbay/src/services/queries/likes.ts
+++ redis-study/rbay/src/services/queries/likes.ts
@@ -1,18 +1,40 @@
 import { client } from '$services/redis';
-import { userLikesKey } from '$services/keys';
+import { userLikesKey, itemsKey } from '$services/keys';
+import { getItems } from './items';
 
-export const userLikesItem = async (itemId: string, userId: string) => {};
 
-export const likedItems = async (userId: string) => {};
+export const userLikesItem = async (itemId: string, userId: string) => {
+    return client.sIsMember(userLikesKey(userId), itemId);
+};
+
+export const likedItems = async (userId: string) => {
+    // Fetch all the item ID's from this user's liked set
+    const ids = await client.sMembers(userLikesKey(userId));
+    
+    // Fetch all the item hashed with those ids and return as array
+    return getItems(ids);
+
+};
 
 export const likeItem = async (itemId: string, userId: string) => {
-    await client.sAdd(userLikesKey(userId), itemId);
+    const inserted = await client.sAdd(userLikesKey(userId), itemId);
+
+    if(inserted){
+        return client.hIncrBy(itemsKey(itemId), 'likes', 1);
+    }
 };
 
 export const unlikeItem = async (itemId: string, userId: string) => {
+    const removed = await client.sRem(userLikesKey(userId), itemId);
 
-    await client.sRem(userLikesKey(userId), itemId);
-
+    if(removed){
+        return client.hIncrBy(itemsKey(itemId), 'likes', -1);
+    }
 };
 
-export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
+export const commonLikedItems = async (userOneId: string, userTwoId: string) => {
+    const ids = await client.sInter([userLikesKey(userOneId), userLikesKey(userTwoId)]);
+
+    return getItems(ids);
+
+};
