a86ec28063df1e8627b9da64efb42236296c22f1
bhsohn <hanmuson1@gmail.com>
2024-06-26 10:04:44 +0900

    아이템 정렬 기능

--- redis-study/rbay/src/services/queries/items/by-views.ts
+++ redis-study/rbay/src/services/queries/items/by-views.ts
@@ -1,10 +1,19 @@
 import { client } from "$services/redis";
 import { itemsKey, itemsByViewsKey } from "$services/keys";
+import { deserialize } from './deserialize';
 
 
 export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
-    const results = await client.sort(itemsByViewsKey(), {
-        GET:['#', `${itemsKey('*')}->name`, `${itemsKey('*')}->views`],
+    let results: any = await client.sort(itemsByViewsKey(), {
+        GET:[
+            '#', 
+            `${itemsKey('*')}->name`,
+            `${itemsKey('*')}->views`,
+            `${itemsKey('*')}->endingAt`,
+            `${itemsKey('*')}->imageUrl`,
+            `${itemsKey('*')}->price`
+        ],
+
         BY: 'nosort', 
         DIRECTION: order,
         LIMIT : {
@@ -13,7 +22,14 @@ export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, c
         }
     });
     
+    const items = [];
+    while(results.length){
+        const [id, name, views, endingAt, imageUrl, price, ...rest] = results
+        const item = deserialize(id, { name, views, endingAt, imageUrl, price });
+        items.push(item);
+        results = rest;
+    }   
 
-    console.log(results);
+    return items;    
 
 };
