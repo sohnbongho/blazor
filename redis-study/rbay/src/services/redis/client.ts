b0d2bf516c234f649fce7f0ba4f616a44ad6532f
bhsohn <hanmuson1@gmail.com>
2024-07-01 17:47:11 +0900

    유저 추가

--- redis-study/rbay/src/services/redis/client.ts
+++ redis-study/rbay/src/services/redis/client.ts
@@ -1,12 +1,34 @@
-import { createClient } from 'redis';
+import { createClient, defineScript } from 'redis';
 
 const client = createClient({
 	socket: {
 		host: process.env.REDIS_HOST,
 		port: parseInt(process.env.REDIS_PORT)
 	},
-	password: process.env.REDIS_PW
+	password: process.env.REDIS_PW,
+	scripts: {
+		addOneAndStore : defineScript({
+			NUMBER_OF_KEYS: 1,
+			SCRIPT : `
+			return redis.call('SET', KEYS[1], 1 + tonumber(ARGV[1]))
+			`,
+			transformArguments(key : string, value: number) {
+				return [key, value.toString()]
+				// ['books:count', '5']
+				// EVALSHA <ID> 1 'books:count '5'
+			},
+			transformReply(reply : any){
+				return reply;
+			}
+		})
+	}
 });
+
+client.on('connect', async () =>{
+	await client.addOneAndStore('books:count', 5)
+	const result = await client.get('books:count');
+	console.log(result)
+})
 
 client.on('error', (err) => console.error(err));
 client.connect();
